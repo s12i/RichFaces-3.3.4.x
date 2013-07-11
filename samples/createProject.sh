cc1cf651715a5619ead63bf18cd006fea34991a1
lfryc <lfryc@46473bb9-a92a-0410-8f14-e4f2f95f0492>
2013-07-11 06:24:44 +0000

    preparing for release: switching version to 3.3.4.Final
    git-svn-id: https://anonsvn.jboss.org/repos/richfaces/branches/community/release-3.3.4@23254 46473bb9-a92a-0410-8f14-e4f2f95f0492

--- samples/createProject.sh
+++ samples/createProject.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mvn archetype:create -DarchetypeGroupId=org.richfaces.cdk -DarchetypeArtifactId=maven-archetype-jsfwebapp \
-         -DarchetypeVersion=3.3.3.Final -Dversion=3.3.3.Final -DgroupId=org.richfaces.samples -DartifactId=$1
+         -DarchetypeVersion=3.3.4.Final -Dversion=3.3.4.Final -DgroupId=org.richfaces.samples -DartifactId=$1
